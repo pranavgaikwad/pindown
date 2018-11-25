0ffe6429c98ff85f34d40445a0ad16506175658c
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2018-11-25 12:33:46 -0500

    added pindown.c

--- .push.sh
+++
@@ -1,7 +0,0 @@
-IP="192.168.10.201"
-# IP="10.152.44.96"
-USERNAME="pranav"
-
-# scp -i /home/pranav/.ssh/id.pranav pindown.c Makefile ${USER}@${IP}:/home/pranav/Desktop/linux-2.6.23/security/
-scp -i /home/pranav/.ssh/id.pranav /home/pranav/Downloads/Pranav_Gaikwad_PinDOWN.tar.gz ${USER}@${IP}:/home/pranav/Desktop/linux-2.6.23/security/
-
